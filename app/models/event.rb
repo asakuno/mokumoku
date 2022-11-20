0526864784be8ae48185a6cca8765e5c90c41060
asakuno <takahiro.asakuno@gmail.com>
2022-11-21 01:15:44 +0900

    [Fix]checkメソッド

--- app/models/event.rb
+++ app/models/event.rb
@@ -21,18 +21,6 @@ class Event < ApplicationRecord
     validates :held_at
   end
 
-  def check(user)
-    check_woman(user) || check_others(user)
-  end
-
-  def check_others(user)
-    self.only_woman == false && user.gender == "lgtm" || self.only_woman == false && user.gender == "man"
-  end
-
-  def check_woman(user)
-    self.only_woman == true && user.gender == "woman" || self.only_woman == false && user.gender == "woman"
-  end
-
   def past?
     held_at < Time.current
   end
